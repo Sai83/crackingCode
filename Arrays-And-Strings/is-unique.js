1850aedf273ff59cde940705f66079ad92ffe3d0
Sai <sai.praneeth@gmail.com>
2021-12-25 11:43:17 -0600

    Using two for loops for same solution

--- Arrays-And-Strings/is-unique.js
+++ Arrays-And-Strings/is-unique.js
@@ -7,12 +7,24 @@
       console.log(value)
 
       if(str.length === value.length){
-          return false;
+          return true;
       }
       
-      return true;
+      return false;
 
     };
+
+    // No set solution iterating between two for loops.
+    const  uniqueCharactersNoSet = (str) => {
+        for(let i=0; i< str.length; i++){
+            for(let j=i+1; j< str.length; j++){
+                return (str[i] === str[j])
+            }
+        }
+    }
+
+
     
-    console.log(uniqueCharacters('abcd'));
+    console.log(uniqueCharacters('abdda'));
+    console.log('No Set', uniqueCharactersNoSet('abdda'))
 })();
