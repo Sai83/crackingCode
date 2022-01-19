69e864fbeef13eb562585cd6a5f14b0c6001d852
Sai <sai.praneeth@gmail.com>
2022-01-19 05:16:27 -0600

    Palindrome solution through hash table

--- Arrays-And-Strings/palendrome-permutation.js
+++ Arrays-And-Strings/palendrome-permutation.js
@@ -4,37 +4,33 @@
  * Output: true taco cat or atco cta
  */
 
+(function(){
 
-const key = (obj) =>{
-    return obj;
-}
+    const palindromePermutation = (str) => {
+        const arrStr1 = [...str];
+        const hash = {};
 
-const palindromePermutation = (str1, str2) => {
-   
-    const arrStr1 = [...str1.toLowerCase()];
-    const arrStr2 = [...str2.toLowerCase()];
-
-    const me = Object.create({});
-    for(let i=0; i< arrStr1.length; i++){
-        let c = arrStr1[i];
-        console.log(c);
-        me.assign(c, i+1);
+        for(let i=0; i<arrStr1.length; i++){
+            let c = arrStr1[i];
+            if(hash[c]){
+                hash[c]++;
+            }else{
+               hash[c] = 1; 
+            }
+        }
+        
+        let count = 0;
+          Object.keys(hash).forEach((key) => {
+            const value = hash[key];
+            count+= value%2;
+        })
+        return count<=1;
+                
     }
-    console.log('Map', map);
-
-  
-    for(let j=0; j<arrStr2.length; j++){
-        let c = arrStr2[j];
-        if(map.get(c)){
-             map.delete(c);
-        }else{
-          return false;
-        } 
-        return true; 
-     }
-   
-}
-
-
-
-console.log(palindromePermutation('Tact Coa', 'super'))
+    
+    
+    
+    
+    console.log(palindromePermutation('aab'))
+    //console.log(palindromePermutation('carerac'))
+})();
